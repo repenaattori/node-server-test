959166ccb1863ae5fef5c8cb5895fda9562a1f4b
repsukka <81526112+repsukka@users.noreply.github.com>
2023-02-16 11:22:01 +0200

    removed mapping

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var app = express();
 
 app.use(express.static('client/build'))
 
-app.get('/', function (req, res) {
-  res.send('Node in render working!');
-});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
