08172af879993761ced514dd50b595105d951817
repsukka <81526112+repsukka@users.noreply.github.com>
2023-05-04 21:53:52 +0300

    init

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 var express = require('express');
-
 var app = express();
+
+const mysql = require('mysql2');
+
+const conf = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'demoDb'
+}
+
+demo();
+
+async function demo(){
+  try{
+    const connection = await mysql.createConnection(conf);
+    await connection.execute('DELETE FROM invoice_items WHERE InvoiceLineID=100');
+  }catch(err){
+    console.log(err);
+  }
+}
+
+
+
+
+async function testi(res){
+  try{
+    const connection = await mysql.createConnection(conf);
+    const [rows, fields] = await connection.execute('SELECT Title FROM albums');
+    if(!rows) 
+      rows=[];
+      console.log(rows);
+    res.json(rows);
+  }catch(err){
+    res.status(500).json({error: err.message})
+  }
+}
+
 
 //app.use(express.static(__dirname+'/build'))
 
-app.get('/test', function (req, res) {
-  res.send('Node in render working!');
+app.get('/test', async function (req, res) {
+  testi(res);
 });
 
 app.get('/', function (req, res) {
---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE customer(
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fname VARCHAR(255),
+    lname VARCHAR(255)
+)
+
+INSERT INTO customer (fname, lname) VALUES ('')
